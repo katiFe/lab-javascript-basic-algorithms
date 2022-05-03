079ba361d5044c202686af2e82362c58a9b6b035
Kati Fe <k-felgitsch@hotmail.com>
2022-05-03 12:09:26 +0200

    review

--- js/index.js
+++ js/index.js
@@ -1,30 +1,41 @@
-//Iteration 1
-let hacker1 = 'Kati';
-console.log(`The drivers name is ${hacker1}`) ;
+let hacker1 = "Kati";
+const newHacker = hacker1.toUpperCase("").split("").join(" ");
+console.log(newHacker);
+console.log("The driver's name is " + hacker1);
+let hacker2= "Kazi";
 
-let hacker2 = 'Bob';
-console.log(`The navigators name is ${hacker2}`);
+const newHacker2 = hacker2.split("").reverse("").join(" ");
+console.log(newHacker2);
+console.log("The navigator's name is " + hacker2);
 
-//Iteration 2
-console.log(`It seems that the navigator has the longest name, it has ${hacker1.length} characters`);
+// //Iteration 2 conditionals
 
-//Iteration 3
-let uppercased = hacker1.toUpperCase();
-console.log(uppercased[0], uppercased[1], uppercased[2], uppercased[3]);
+console.log(hacker1.length);
 
-let str = 'Kati';
-let strReverse = str.split('').reverse().join('');  
-console.log(`${strReverse}`);
-
-console.log(hacker1);
-
-const hacker = ['Kati', 'Bob'];
-console.log(hacker.sort());
-
-if (hacker.sort[0] > hacker.sort[1]) {
-  console.log(`Yo, the navigator goes first definitely`);
-} else if (hacker.sort[0] < hacker.sort[1]) {
-  console.log(` The driver's name goes first.`);
-} else {
-  console.log(`What?! You both have the same name?`)
+if(hacker1.length > hacker2.length){
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+}else if (hacker1.length < hacker2.length){
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length}
+     characters.`)
+}else{
+    console.log(`Wow, you both have equally long names, ${hacker2.length} characters!`)
 }
+
+//Iteration 3 to sort the names alphabetically
+let items = [hacker1, hacker2];
+const namesInOrder = items.sort((a, b) => a.localeCompare(b));
+console.log(namesInOrder);
+
+
+//to print the desired messages depending on the order of the names
+if(hacker1 === hacker2) {
+  console.log("What?! You both have the same name?")
+} else if (namesInOrder[0] === hacker1) {
+  console.log("The driver's name goes first.")
+} else {
+  console.log("Yo, the navigator goes first definitely.")
+}
+
+
+
+
